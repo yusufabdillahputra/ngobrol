a81c087ce7267a366bb91d408eaad930e73b6425
yusufabdillahputra <yusufabdillahputra@gmail.com>
2019-12-27 21:53:33 +0700

    Add Maps View

--- src/Screens/MapFriend.js
+++ src/Screens/MapFriend.js
@@ -16,50 +16,125 @@ import {
   Title,
   Button,
   Icon,
+  Text,
 } from 'native-base';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
+import { db } from '../Utils/Services/initialize';
+import ScreenLoading from '../Components/Loading/ScreenLoading';
 
 class MapFriend extends Component {
-  render () {
-    return (
-      <Container>
-        <Header
-          style={BGColors.primary}
-          androidStatusBarColor={RawColors.primary}
-        >
-          <Left
-            style={{
-              flex: 0.5,
-            }}
-          >
-            <Button
-              transparent
-              iconRight
-              onPress={
-                () => this.props.navigation.replace('HomeScreen')
-              }
-            >
-              <Icon
-                type='MaterialIcons'
-                name='arrow-back'
-              />
-            </Button>
-          </Left>
-          <Body
-            style={{
-              flex: 1,
-            }}
-          >
-            <Title>
-              Location Friend
-            </Title>
-          </Body>
-        </Header>
-        <Content>
 
-        </Content>
-      </Container>
-    );
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      friendList: [],
+    };
+
+  }
+
+  async componentDidMount () {
+    await db().ref('users').on('value', async snapshot => {
+      await this.setState({
+        isLoading: false,
+        friendList: Object.values(snapshot.val()),
+      });
+    });
+  }
+
+  render () {
+    if (this.state.isLoading) {
+      return <ScreenLoading/>;
+    } else {
+      return (
+        <>
+          <Header
+            style={BGColors.primary}
+            androidStatusBarColor={RawColors.primary}
+          >
+            <Left
+              style={{
+                flex: 0.5,
+              }}
+            >
+              <Button
+                transparent
+                iconRight
+                onPress={
+                  () => this.props.navigation.replace('HomeScreen')
+                }
+              >
+                <Icon
+                  type='MaterialIcons'
+                  name='arrow-back'
+                />
+              </Button>
+            </Left>
+            <Body
+              style={{
+                flex: 1,
+              }}
+            >
+              <Title>
+                Location Friend
+              </Title>
+            </Body>
+          </Header>
+          <View style={styles.container}>
+            <MapView
+              provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+              style={styles.map}
+              region={{
+                latitude: -6.620422,
+                longitude: 106.8184278,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+              }}
+            >
+              {
+                this.state.friendList.length !== 0
+                  ? this.state.friendList.map((item, index) => {
+                    return (
+                      <Marker
+                        key={index}
+                        title={item.displayName}
+                        onCalloutPress={
+                          () => this.props.navigation.navigate('ProfileFriendScreen', {
+                            uid : item.uid_users
+                          })
+                        }
+                        coordinate={{
+                          latitude: Number(item.latitude),
+                          longitude: Number(item.longitude),
+                        }}
+                      />
+                    )
+                  })
+                  : null
+              }
+            </MapView>
+          </View>
+        </>
+      );
+    }
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    marginTop: 58,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default MapFriend;
